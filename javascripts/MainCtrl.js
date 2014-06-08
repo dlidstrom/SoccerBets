ffe833742c34a4605b0528c6b6bfb01e6c3f2e08
Daniel Lidstrom <dlidstrom@gmail.com>
2014-06-08 17:24:59 +0200

    Maybe it all works now...

--- javascripts/MainCtrl.js
+++ javascripts/MainCtrl.js
@@ -32,35 +32,40 @@
                 };
 
                 $scope.winnerChanged = function (winner) {
-                    storage.setWinner(winner);
+                    controller.winnerRef.update(winner);
                 };
 
                 $scope.optionChanged = function ($index, result) {
-                    var ref = controller.ref.child('match-' + ($index < 10 ? '0' + $index : $index));
+                    var ref = controller.matchesRef.child('match-' + ($index < 10 ? '0' + $index : $index));
                     ref.update({result: result});
                 };
 
                 var controller = this;
                 $rootScope.$on('events:login', function (e, user) {
                     var fb = new Firebase(FIREBASE_URL);
-                    controller.ref = fb.child('users').child(user.id).child('matches');
-                    $scope.matches = $firebase(controller.ref);
-                });
+                    controller.matchesRef = fb.child('users').child(user.id).child('matches');
+                    controller.winnerRef = fb.child('users').child(user.id).child('winner');
+                    $scope.matches = $firebase(controller.matchesRef);
+                    $scope.storedWinner = $firebase(controller.winnerRef);
 
-                $scope.teams = _($scope.matches)
-                    .uniq('homeTeam')
-                    .pluck('homeTeam')
-                    .sort()
-                    .map(function (n) {
-                        return { name: n };
-                    })
-                    .value();
-                var storedWinner = storage.getWinner('');
-                if (storedWinner) {
-                    $scope.winner = _($scope.teams)
-                        .find(function (t) {
-                            return t.name === storedWinner.name;
+                    controller.matchesRef.on('value', function (snapshot) {
+                        $scope.teams = _(snapshot.val())
+                            .pluck('homeTeam')
+                            .uniq()
+                            .sort()
+                            .map(function (n) {
+                                return { name: n };
+                            })
+                            .value();
+                        controller.winnerRef.once('value', function (winnerSnap) {
+                            var winner = winnerSnap.val();
+                            if (!winner) return;
+                            $scope.winner = _($scope.teams)
+                                .find(function (t) {
+                                    return t.name === winner.name;
+                                });
                         });
-                }
+                    });
+                });
         }]);
 })(window.App);

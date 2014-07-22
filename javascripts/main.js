c4cca3cab662f13484697b484b336fad533bbe73
Daniel Lidström <dlidstrom@gmail.com>
2014-07-22 16:01:17 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 (function (w) {
     var app = angular.module('App', ['firebase']);
-    app.constant('DEADLINE', moment('2014-06-12T22:00'));
+    app.constant('DEADLINE', moment('2018-06-15T18:00'));
     app.constant('FIREBASE_URL', 'https://worldcup-2014.firebaseio.com/');
     w.App = app;
 })(window);
